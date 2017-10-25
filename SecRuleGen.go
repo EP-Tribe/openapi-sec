51a8d9c119ce84eefa515064c718c78555ea6d5d
EP\gilles.huet <gilles.h@ep.fr>
2017-10-25 16:55:51 +0200

    no message

--- SecRuleGen.go
+++ SecRuleGen.go
@@ -22,11 +22,22 @@ type Config struct {
 
 type Endpoint struct {
     Url string
-    Methods []string
+    Methods []Method
 }
 
-type EndpointMethodConf struct {
-    Summary string `json:"summary"`
+type Method struct {
+	Name string
+	Parameters []EndpointParameter
+}
+
+type EndpointParameter struct {
+    Name string `json:"name"`
+    In string `json:"in"`
+    Description string `json:"description"`
+    Format string `json:"format"`
+    Type string `json:"type"`
+    Required bool `json:"required"`
+    Enum []string `json:"enum"`
 }
 
 func toJson(p interface{}) string {
@@ -58,7 +69,6 @@ func getSwaggerSpec(s string) interface{} {
         os.Exit(1)
     }
     body, err := ioutil.ReadAll(response.Body)
-//    var swaggerSpec SwaggerSpec
     var swaggerSpec interface{}
     json.Unmarshal(body, &swaggerSpec)
     return swaggerSpec
@@ -66,30 +76,61 @@ func getSwaggerSpec(s string) interface{} {
 
 func getEndpointList(s map[string]interface{}) []Endpoint {
     var list []Endpoint
-    //list.Endpoints[0].Url = "test"
-    for k1, v1 := range s {
-        if k1 == "paths" { 
-            for k2, v2 := range v1.(map[string]interface{}) {
-                var methods []string
-                for k3, _ := range v2.(map[string]interface{}) {
-                    methods = append(methods, k3)
-                }
-                list = append(list, Endpoint{k2, methods })   
-            }
-        }
-
+    for endpoint, availableMethods := range s["paths"].(map[string]interface{}) {
+    	var methods []Method
+    	var parameters []EndpointParameter
+    	for method, description := range availableMethods.(map[string]interface{}){
+    		for k, v := range description.(map[string]interface{}){
+    			if k == "parameters" {
+    				switch x := v.(type) {
+						case []interface{}:
+    						for _, e := range x {
+        						fmt.Println(e,"\n")
+        						var p EndpointParameter
+        						s, _ := json.Marshal(e)
+        						json.Unmarshal(s, &p)
+        						parameters = append(parameters, p)
+        						fmt.Println(p.Name)
+    						}
+						default:
+    						fmt.Printf("can't parse parameter %T\n", v)
+					}
+    			}
+    		}
+    		methods = append(methods, Method{method, parameters})
+    	}
+    	list = append(list, Endpoint{endpoint, methods})
     }
     return list
 }
 
 func main() {
+	config := readConfigFile("C:\\Users\\gilles.huet\\Documents\\swagger-mod_security\\test.json")
 //    if len(os.Args) != 2 {
-        config := readConfigFile("C:\\Users\\joanelis\\Desktop\\tools\\GoSecGen\\test2.json")
 //        fmt.Fprintf(os.Stderr, "Usage: %s config.json\n", os.Args[0])
 //        os.Exit(1)
 //    }
 //    config := readConfigFile(os.Args[1])
     swaggerSpecs := getSwaggerSpec(config.Url).(map[string]interface{})
     fmt.Println("Version of swagger specifications : ", swaggerSpecs["swagger"], "\nParsing its content...\n")
-    fmt.Println(getEndpointList(swaggerSpecs))
+    endpoints := getEndpointList(swaggerSpecs)
+    fmt.Println(endpoints)
+    for _, endpoint := range endpoints {
+    	fmt.Println("\nendpoint : ", endpoint.Url)
+    	for _, method := range endpoint.Methods {
+    		fmt.Println("\t\t", method.Name)
+    		fmt.Println("\t\t\tparameters : ")
+    		for _, parameter := range method.Parameters {
+    			var mappedParameters map[string]string
+    			p, _ := json.Marshal(parameter)
+    			json.Unmarshal(p, &mappedParameters)
+    			for k, v := range mappedParameters {
+    				if len(v) > 0 {
+    					fmt.Println("\t\t\t\t", k, " : ", v)
+    				}
+    			}
+    			fmt.Println("\n")
+    		}
+    	}
+    }
 }
